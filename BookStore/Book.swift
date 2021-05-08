ed36d2d57455b9242a3e92b1c44310bb43b99672
user190373 <user190373@TX035.local>
2021-05-07 19:30:16 -0500

    Challenge 1

---
+++ BookStore/Book.swift
@@ -0,0 +1,19 @@
+//
+//  Book.swift
+//  BookStore
+//
+//  Created by user190373 on 3/5/21.
+//
+
+import Foundation
+
+class Book{
+    var title : String = ""
+    var author : String = ""
+    var description : String = ""
+    var year : String = ""
+    var pages: Int = 0
+    var readThisBook: Bool = false
+    
+}
+
